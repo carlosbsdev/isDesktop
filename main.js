5568a3b7896170ad5ca15a9b74c84472d7ceda93
Charly <carlos11811@gmail.com>
2016-03-30 21:44:32 +0200

    Added files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function esDispositivoMovil() {
+    if (/Android|android|webOS|webos|iPhone|iphone|iPad|ipad|iPod|ipod|BlackBerry|blackBerry|IEMobile|iemobile|opera mini|Opera Mini/i.test(navigator.userAgent)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if(esDispositivoMovil()){
+    alert('Móvil');
+}else{
+    alert('Desktop')
+}
